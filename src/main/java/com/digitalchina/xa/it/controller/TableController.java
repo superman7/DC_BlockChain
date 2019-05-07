0fbc038df58f3de098788705ddb355238aca8f62
DrogonFlykeeperDC <754826551@qq.com>
2019-05-07 16:08:14 +0800

    1.将之前使用jdbc建表和插入数据改为动态链接数据库。2.修复页面Bug

--- src/main/java/com/digitalchina/xa/it/controller/TableController.java
+++ src/main/java/com/digitalchina/xa/it/controller/TableController.java
@@ -32,15 +32,11 @@ import com.digitalchina.xa.it.util.DecryptAndDecodeUtils;
 import com.digitalchina.xa.it.util.HttpRequest;
 import com.digitalchina.xa.it.util.JDBCUtils;
 import com.digitalchina.xa.it.util.TConfigUtils;
-import com.mysql.fabric.xmlrpc.base.Data;
 
 @Controller
 @RequestMapping(value = "/table")
 public class TableController {
-	//动态链接数据库所需参数
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	// URL编写方式：jdbc:mysql://主机名称：连接端口/数据库的名称?参数=值
-	private static final String URL = "jdbc:mysql://10.0.5.106:4001/";
+	
 	private static final String PASSWORD = "0x189a";
 	
 	@Autowired
@@ -49,13 +45,11 @@ public class TableController {
 	@GetMapping("/createTable")
 	@Transactional
 	public Map<String, Object> getData(
-			@RequestParam(name = "param", required = true) String param){
+			@RequestParam(name = "param", required = true) String param) throws ClassNotFoundException, SQLException{
 		
 		String jsonValue = param.trim();
 		System.out.println(jsonValue);
 		Map<String, Object> modelMap = DecryptAndDecodeUtils.decryptAndDecode(jsonValue);
-		System.out.println(modelMap.get("success"));
-		System.out.println(modelMap);
 		JSONObject jsonObj = JSONObject.parseObject((String) modelMap.get("data"));
 		System.out.println(jsonObj);
 		System.out.println(jsonObj.size());
@@ -66,6 +60,7 @@ public class TableController {
 		}
 		String tableName = jsonObj.getString("tableName");
 		String itcode = jsonObj.getString("itcode");
+		System.out.println(itcode);
 		System.out.println("查找数据库中有无重名表");
 		List<Map<String,Object>> list = jdbc.queryForList("select table_name from information_schema.tables where table_schema='dc_blockchain' and table_name = '"+tableName+"'");
 		System.out.println(list.size());
@@ -74,13 +69,14 @@ public class TableController {
 			modelMap.put("success", false);
 			return modelMap;
 		}
+		String sql = "CREATE TABLE "+tableName+" ("
+				+ field.substring(0, field.length()-1)
+				+")";
 		System.out.println("CREATE TABLE "+tableName+" ("
 				+ field.substring(0, field.length()-1)
 				+")");
-		jdbc.execute("CREATE TABLE "+tableName+"("
-				+ field.substring(0, field.length()-1)
-				+")"
-				);
+		//使用工具类动态链接数据库
+		JDBCUtils.executeSQL(sql, itcode, PASSWORD);
 		System.out.println("将操作记录记录至建表信息表中"+"INSERT INTO table_info (itcode,table_name,table_status,fields,create_time)"
 				+ "VALUES('"+itcode+"','"+tableName+"',"+0+",'"+field.substring(0, field.length()-1)+new Timestamp(new Date().getTime())+"')");
 		jdbc.execute("INSERT INTO table_info (itcode,table_name,table_status,fields)"
@@ -125,11 +121,13 @@ public class TableController {
 	public Map<String, Object> addDataToTable(@RequestParam(name = "tableName",required = true)String tableName,
 			@RequestParam(name = "itcode",required = true)String itcode,
 			@RequestParam(name = "fieldNames",required = true)String fieldNames,
-			@RequestParam(name = "fieldValues",required = true)String fieldValues){
+			@RequestParam(name = "fieldValues",required = true)String fieldValues) throws ClassNotFoundException, SQLException{
 		HashMap<String,Object> map = new HashMap<>();
 		System.out.println("插入数据到表中"+"INSERT INTO "+tableName+"("+fieldNames+") VALUES ("+fieldValues+")");
 		String data = "INSERT INTO "+tableName+"("+fieldNames+") VALUES ("+fieldValues+")";
-		int result = jdbc.update(data);
+		//int result = jdbc.update(data);
+		//使用工具类动态链接数据库
+		int result = JDBCUtils.executeSQL(data, itcode, PASSWORD);
 		System.out.println(result);
 		if (result == 0) {
 			map.put("success", false);
